407cc78edcc4f65daf64739ed18ff33e4c50d3d5
Sergei Petrunia <psergey@askmonty.org>
2020-03-09 00:34:34 +0300

    10-sort-int-overhead.sh

---
+++ 10-setup-trees-reg1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./setup-server/setup-mariadb-current.sh 10.5-mdev21784-reg1-base
+./setup-server/setup-mariadb-current.sh 10.5-mdev21784-reg1-vfuncs
+
+
---
+++ 10-sort-int-overhead.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+RES='result-sort-int'
+
+mkdir $RES
+
+bash filesort-bench1/01-make-sort-int-bench.sh > $RES/sort-int-bench.sql
+
+for SERVER in 10.5-mdev21784-reg1-base 10.5-mdev21784-reg1-vfuncs ; do
+  
+  (cd $SERVER; git log -1) > $RES/tree-$SERVER.txt
+
+  bash prepare-server.sh -m $SERVER
+  source $SERVER-vars.sh
+
+  $MYSQL $MYSQL_ARGS test < $RES/sort-int-bench.sql | tee $RES/sort-int-$SERVER.txt
+
+  echo "TEST_RUN:,$SERVER" >> $RES/summary.txt
+  tail -n 10 $RES/sort-int-$SERVER.txt >> $RES/summary.txt
+done
+
+cat $RES/summary.txt
+
+
+
+
+
