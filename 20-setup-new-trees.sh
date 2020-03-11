2d1fa80200411f92e84ca1f9aedd9868835b4b31
Sergei Petrunia <psergey@askmonty.org>
2020-03-11 12:52:34 +0300

    Re-run the benchmark on the final patch

---
+++ 20-setup-new-trees.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./setup-server/setup-mariadb-current.sh 10.5 b753ac066bc26acda9deb707a31c112f1bbf9ec2 10.5-new
+./setup-server/setup-mariadb-current.sh 10.5 561b5ce3647a63c343f70ce183e408423b3fa65c 10.5-old
+
+
---
+++ 21-run-varch-bench-again.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+RES='result-varchars'
+
+mkdir $RES
+
+bash filesort-bench1/06-make-varchar-bench.sh > $RES/varchar-bench.sql
+
+for SERVER in 10.5-old 10.5-new ; do
+  
+  (cd $SERVER; git log -1) > $RES/tree-$SERVER.txt
+
+  bash prepare-server.sh -m $SERVER
+  source $SERVER-vars.sh
+  echo $SERVER
+  $MYSQL $MYSQL_ARGS test < $RES/varchar-bench.sql | tee $RES/varchar-$SERVER.txt
+
+done
