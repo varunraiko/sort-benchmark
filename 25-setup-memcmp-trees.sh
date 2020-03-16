9f187fd724c7c1c0217fe5fecf4e7ef361579497
Sergei Petrunia <psergey@askmonty.org>
2020-03-16 23:00:03 +0300

    Test MyRocks-style encoding

---
+++ 25-setup-memcmp-trees.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#./setup-server/setup-mariadb-current.sh 10.5 b753ac066bc26acda9deb707a31c112f1bbf9ec2 10.5-new
+#./setup-server/setup-mariadb-current.sh 10.5 561b5ce3647a63c343f70ce183e408423b3fa65c 10.5-old
+
+./setup-server/setup-mariadb-current.sh 10.5-mdev21955 d187214b884f3f37eda052dacc701eece6284183 10.5-memcmp
+
+
